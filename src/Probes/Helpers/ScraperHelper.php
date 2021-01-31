7778823a745dd881a81f0e5db63adb7f15a4cd95
twittingeek <palma.giacomo@gmail.com>
2021-01-31 10:07:48 +0100

    remove debug options from chromium

--- src/Probes/Helpers/ScraperHelper.php
+++ src/Probes/Helpers/ScraperHelper.php
@@ -49,9 +49,8 @@ class ScraperHelper
         $browser = $browserFactory->createBrowser(
             [
                 'startupTimeout' => 120,
-                'connectionDelay' => 3.8,
                 'sendSyncDefaultTimeout' => 60000,
-                'debug' => true,
+                'debug' => false,
                 'noSandbox' => true,
                 'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15',
             ]);
